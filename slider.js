188f2f128791d0110b541246c86f9eca20de1ac5
Dasha Bezkrovna <rastya3012gmail.com>
2024-09-10 13:55:18 +0300

    все части готовы, кроме мелких вопросов

--- slider.js
+++ slider.js
@@ -2,8 +2,8 @@
 const slider = document.querySelector('.swiper-container');
 
 let mySwiper = new Swiper(slider, {
-	slidesPerView: 2, // сколько слайдов мі видим
-	spaceBetween: 20, //расстояние между слайдами
+	slidesPerView: 2.4, // сколько слайдов мі видим
+	spaceBetween: 26, //расстояние между слайдами
 	loop: true, //зацыкленость слайда, нет конца с прави и лева
 
 	pagination: {
@@ -19,6 +19,25 @@ let mySwiper = new Swiper(slider, {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-})
+	on: {
+		slideChange: function () {
+			highlightActiveSlide(this);
+		},
+	},
+});
+
+function highlightActiveSlide(swiper) {
+	const slides = document.querySelectorAll('.swiper-slide');
+	slides.forEach(slide => slide.classList.remove('active-slide')); // Удаляем класс у всех слайдов
+	const activeIndex = swiper.activeIndex;
+	const activeSlide = slides[activeIndex];
+	if (activeSlide) {
+		activeSlide.classList.add('active-slide'); // Добавляем класс к активному слайду
+	}
+}
+
+// Инициализация начального состояния
+highlightActiveSlide(swiper);
+
 
 // чтобы менять ширину контейнера, нужно оменять ширину контейнера, а не карточки
